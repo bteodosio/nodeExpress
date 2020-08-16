d5767ed6055eee5ab5c070e158755155060a2734
bteodosio <teodosio.goncalves@gmail.com>
2020-08-15 21:26:59 -0300

    Alteracao dos diretorios

---
+++ src/config/databaseConfig.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'mysql',
+    host: 'pitstop.mysql.uhserver.com',
+    username: 'bteodosio',
+    password: 'br902707!@',
+    database: 'pitstop',
+    define: {
+        timestamps: true,
+        underscored: true
+    }
+};
